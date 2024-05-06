620d59be339d7879ac63a4e714ddce27337f3ae3
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-05-06 11:37:24 -0500

    Implemented templates, and routes

--- newapp/views.py
+++ newapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-  return HttpResponse("Hello world")
+  # Rendering the html template
+  return render(request, "index.html")
