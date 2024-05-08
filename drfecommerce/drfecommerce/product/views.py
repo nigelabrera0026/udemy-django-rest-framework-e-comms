5e465ceed30df319379247874ccc67aef927adb9
nigelabrera0026 <abrera.nigel0026@gmail.com>
2024-05-08 08:51:13 -0500

    Configured coverage, and implementing unit and end to end test

--- drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/product/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Category, Brand, Product
+from .serializers import CategorySerializer, BrandSerializer, ProductSerializer
+
+from drf_spectacular.utils import extend_schema
 # Create your views here.
 
 class CategoryViewSet(viewsets.ViewSet):
@@ -11,6 +13,29 @@ class CategoryViewSet(viewsets.ViewSet):
   """
   queryset = Category.objects.all()
 
+  @extend_schema(responses=CategorySerializer)
   def list(self, request):
     serializer = CategorySerializer(self.queryset, many=True)
+    return Response(serializer.data)
+
+class BrandViewSet(viewsets.ViewSet):
+  """
+  A simple viewset for viewing all brands
+  """
+  queryset = Brand.objects.all()
+
+  @extend_schema(responses=BrandSerializer)
+  def list(self, request):
+    serializer = BrandSerializer(self.queryset, many=True)
+    return Response(serializer.data)
+
+class ProductViewSet(viewsets.ViewSet):
+  """
+  A simple viewset for viewing all Product
+  """
+  queryset = Product.objects.all()
+
+  @extend_schema(responses=ProductSerializer)
+  def list(self, request):
+    serializer = ProductSerializer(self.queryset, many=True)
     return Response(serializer.data)
--- drfecommerce/drfecommerce/test/test_example.py
+++
@@ -1,5 +0,0 @@
-# Creating sample test
-
-def test_example():
-  assert 1 == 1
-  
